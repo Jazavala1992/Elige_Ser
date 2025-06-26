e5a3dc0e98827bc23fa24959755b6e97d0c75105
Jose Zavala <jazavala@umsa.bo>
2025-06-26 12:09:47 -0400

    Remove hardcoded password from db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import {createPool} from 'mysql2/promise';
 export const pool = createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'J.zavala1992',
+    password: process.env.DB_PASSWORD || '', // Quitar tu contraseña real
     database: process.env.DB_NAME || 'ElijeSer',
     port: process.env.DB_PORT || 3306,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
